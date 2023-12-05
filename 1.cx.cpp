af39c4930d30b6e3516a183dfab2ecb8ebac133f
Harsh-Mangukiya <singalaakshank98@gmail.com>
2023-12-05 18:04:50 +0530

    lec 5.1...

---
+++ 1.cx.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+main(){
+	
+	int a,b;
+	clrscr();
+	printf("enter value a :");
+	scanf("%d",&a);
+	printf("eneter value b:");
+	scanf("%d",&b);
+	if(a<b){
+		printf("a is big ");
+		}else{
+		printf ("b is big ");
+		}
+		getch();
+	}
+		
+	
+
---
+++ 2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+ main(){
+	int n;
+	clrscr();
+	printf("enetr any number : ");
+	scanf("%d",&n);
+	if(0<=n){
+		printf(" natural and positive ");
+	}else  {
+		printf(" non natural and negative");
+	}
+		getch();
+} 
+
